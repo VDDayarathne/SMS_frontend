c25b072bd5da1363ee7f0f930a8183da6fd472a6
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2024-05-31 00:20:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>SMS</h1>
+        <p>Sport_Management_System</p>
+      </header>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,6 @@
+import { getDefaultNormalizer } from "@testing-library/react"
+
+function Main(){
+    return(<div>This is main component</div>)
+}
+export default Main;
