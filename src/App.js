d5691d07bf45b107f170bd7a54ba94f1486373b0
VDDayarathne <hgvddayarathne@std.appsc.sab.ac.lk>
2024-06-03 02:42:56 +0530

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Main from './components/Main';
+// Assuming navbar is a component, you should import it correctly
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>SMS</h1>
-        <p>Sport_Management_System</p>
-      </header>
+      <Dashboard />
       <Main />
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import { getDefaultNormalizer } from "@testing-library/react"
+import React from 'react';
 
 function Main(){
-    return(<div>This is main component</div>)
+    return(<div></div>)
 }
 export default Main;
